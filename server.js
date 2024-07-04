ec1a8419e5aad7fd6235db52cc047e931e794dcd
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-07-04 23:36:35 +0100

    changed the limit size of json files

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({limit : '100mb'}));
+app.use(express.urlencoded({limit : '100mb', extended : true }));
 
 // Routes
 app.use('/api/web', webRoutes);
