b0d6a8014fa07b14929471acd5e66426a39c84cd
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-07-05 00:45:53 +0100

    final -3

--- DB/connect.js
+++ DB/connect.js
@@ -8,7 +8,10 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 1000,  // Set a high connection limit, but not unlimited
+    queueLimit: 0
 }).promise();
 
 export default pool;
