514ea9037526265ed4e4db805464fe03cefac487
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-07-08 16:13:28 +0100

    Revert "PFEChakib server"
    This reverts commit 476ff167524d5dafc3725d4600e414d4fe8a325d.

--- Controllers/Web/deleteTrajet.js
+++ Controllers/Web/deleteTrajet.js
@@ -1,17 +1,5 @@
 import pool from '../../DB/connect.js';
 import { StatusCodes } from 'http-status-codes';
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
-
-import {extractPublicId , deleteImageFromCloudinary} from '../../Util/cloudinaryDelete.js';
-
-dotenv.config();
-
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET 
-});
 
 const deleteCamion = async (req , res) => {
 
@@ -21,29 +9,11 @@ const deleteCamion = async (req , res) => {
         return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Please provide camion_id' });
     }
 
-    const [chauffeurRows] = await pool.query('SELECT photo_conducteur FROM chauffeur WHERE camion_id = ?', [camion_id]);
-        await Promise.all(chauffeurRows.map(async (row) => {
-            if (row.photo_conducteur) {
-                const publicId = extractPublicId(row.photo_conducteur);
-                if (publicId) {
-                    await deleteImageFromCloudinary(publicId, 'conducteur_photos');
-                }
-            }
-        }));
+    const sql = 'DELETE FROM chauffeur WHERE camion_id = ?' ;
+    const result = await pool.query(sql,[camion_id]);
 
-        // Fetch and delete pictogramme for the given camion_id
-        const [matiereRows] = await pool.query('SELECT pictogramme FROM matiere WHERE camion_id = ?', [camion_id]);
-        await Promise.all(matiereRows.map(async (row) => {
-            if (row.pictogramme) {
-                const publicId = extractPublicId(row.pictogramme);
-                if (publicId) {
-                    await deleteImageFromCloudinary(publicId, 'matiere_pictogrammes');
-                }
-            }
-        }));
-
-    await pool.query('DELETE FROM chauffeur WHERE camion_id = ?', [camion_id]);
-    await pool.query('DELETE FROM matiere WHERE camion_id = ?', [camion_id]);
+    const sql2 = 'DELETE FROM matiere WHERE camion_id = ?' ;
+    const result2 = await pool.query(sql2,[camion_id]);
     
     return res.status(StatusCodes.OK).json({ message: 'Trajet deleted successfully' });
 
